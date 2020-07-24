6bc294d4823dc41ac8e9cc78dd050fef8e06ed30
samridhijain <38701845+samridhijain@users.noreply.github.com>
2020-07-24 06:10:33 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/ui/second_app1.dart';
+import 'package:second_app/ui/second_app2.dart';
+import 'ui/test.dart';
+import 'ui/home.dart';
+import 'ui/profile.dart';
+
+main() {
+  runApp(mysecond_app2());
+}
